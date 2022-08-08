b056c2978f0361b78031b4d12c9c9f260c7b85aa
Michal Hradis <michal.hradis@gmail.com>
2022-08-08 09:57:45 +0200

    Parallel html parsing.

--- parse_discussion.py
+++ parse_discussion.py
@@ -2,6 +2,7 @@ import os
 import argparse
 from collections import defaultdict
 from bs4 import BeautifulSoup
+from multiprocessing import Pool
 
 
 def parse_arguments():
@@ -14,6 +15,26 @@ def parse_arguments():
     return args
 
 
+class Extractor(object):
+    def __init__(self, output_path, input_path):
+        self.output_path = output_path
+        self.input_path = input_path
+
+    def __call__(self, params):
+        article = params[0]
+        article_files = params[1]
+        output_file = os.path.join(self.output_path, article + '.txt')
+        with open(output_file, 'w', encoding='utf-8') as output_file:
+            for file in article_files:
+                with open(os.path.join(self.input_path, file), 'r', encoding='windows-1250') as input_file:
+                    soup = BeautifulSoup(input_file.read(), 'html.parser')
+                for div in soup.find_all('div', {"class": "user-text"}):
+                    text = div.getText().strip()
+                    text = text.encode('utf-8', 'replace').decode()
+                    print(text, file=output_file)
+
+        return article_files
+
 def main():
     args = parse_arguments()
 
@@ -23,16 +44,14 @@ def main():
         article = '.'.join(file.split('.')[:-2])
         articles[article].append(file)
 
-    for article in articles:
-        output_file = os.path.join(args.output_path, article + '.txt')
-        with open(output_file, 'w', encoding='utf-8') as output_file:
-            for file in articles[article]:
-                with open(os.path.join(args.input_path, file), 'r', encoding='windows-1250') as input_file:
-                    soup = BeautifulSoup(input_file.read(), 'html.parser')
-                for div in soup.find_all('div', {"class": "user-text"}):
-                    text = div.getText().strip()
-                    text = text.encode('utf-8', 'replace').decode()
-                    print(text, file=output_file)
+    items = list(articles.items())
+
+    try:
+        pool = Pool(12)
+        print(pool.map(Extractor(args.output_path, args.input_path), items))
+    finally:
+        pool.close()
+        pool.join()
 
 
 if __name__ == '__main__':
